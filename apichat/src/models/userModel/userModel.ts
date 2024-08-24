4cd59ffc709f46a50e47a30b3e1362d0587f3561
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 09:41:21 -0600

    [FEAT] Creando la entidad UserEntity
    - creando la clase UserEntity con las columnas id, name, middlename, paternalsurname, maternalsurname, birthdate, email y phone
    - con esto estamos lo grando emular la entidad de la base de datos

--- apichat/src/models/userModel/userModel.ts
+++ apichat/src/models/userModel/userModel.ts
@@ -1,1 +1,28 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  middlename!: string;
+
+  @Column()
+  paternalsurname!: string;
+
+  @Column()
+  maternalsurname!: string;
+
+  @Column()
+  birthdate!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  phone!: string;
+}
