87411fc2d8b2a5177decc81c40651e08f540a507
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 17:12:52 -0600

    [FEAT] Desestructuramos el name y el value del objeto event.target

--- appchat/src/hooks/useForm.ts
+++ appchat/src/hooks/useForm.ts
@@ -5,9 +5,9 @@ export const useForm = (initialForm) =>{
     const [form, setform] = useState(initialForm)
     
     const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
-
+        const { name, value } = e.target
     } 
-    
+
     return {
 
     }
