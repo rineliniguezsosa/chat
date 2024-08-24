b104985556bb5b1e25d11d86d1593dd2a39768c3
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 14:41:13 -0600

    [FEAT] Cambios de css en el componente FormWrapper, estableciendo un padding interno de 0.6rem en la terjeta gris donde esta nuestro contenido del formulario

--- appchat/src/components/FormWrapper.tsx
+++ appchat/src/components/FormWrapper.tsx
@@ -7,7 +7,7 @@ export const FormWrapper = ({title,children}:FormWrapperProps) => {
     <div style={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'row',padding:'1rem'}}>
         <MuiAvatar/>
 
-        <div style={{border:'1px blue solid',width:'100%',borderRadius:'10px',backgroundColor:'#f0f0f0',marginLeft:'0.4rem'}}>
+        <div style={{border:'1px blue solid',width:'100%',borderRadius:'10px',backgroundColor:'#f0f0f0',marginLeft:'0.4rem',padding:'0.6rem'}}>
             <h2>{title}</h2>
             <form>
                 {children}
