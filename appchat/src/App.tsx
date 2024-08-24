52452c48b42544a6b0ebf14f707da9b7240a4c4b
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 12:23:30 -0600

    [FEAT] Estableciendo el icono del reloj y el titulo de en menos de 5 minutos
    - importando el icono AccessTimeIcon
    - estableciendo el elemento html div el cual envuelve el icono
    - estableciendo el elemento span con el titulo "En menos de 5 minutos"
    - voy a tomar un break necesito ir por el agua, ahora regreso

--- appchat/src/App.tsx
+++ appchat/src/App.tsx
@@ -1,4 +1,5 @@
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 function App() {
   return (
@@ -6,9 +7,12 @@ function App() {
       <header>
         <div style={{border:'2px green solid',display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
          <div>
-            <h1>Crear usuario</h1>
+            <h1 style={{fontFamily:'sans-serif'}}>Crear usuario</h1>
          </div>
          <ContentPasteIcon style={{color:'white',fontSize:'5rem'}}/>
+        </div>
+        <div>
+          <AccessTimeIcon/><span>En menos de 5 minutos</span>
         </div>
       </header>
     </main>
