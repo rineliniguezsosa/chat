68bc79f68a05ddc994cdd9769b3d3b94ea113067
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 17:26:50 -0600

    [FEAT] Muliples cambios en el useForm.ts
    - creamos la interface FormState donde establecemos que cualquier objeto cuya implementación puede tener claves de tipo string
    - cambios en la función useForm usamos el generico que va a extender a FormState donde esperamos palabras un objeto con palabras claves, esto lo investigue
    - actualizamos el valor del state form donde de vamos a propagar los valores anteriores y usamos propiedades computadas para actualizar los valores mediante llaves y valores
    - devolviendo del custom hook useForm la variable form y la referencia de la función handleInputChange

--- appchat/src/hooks/useForm.ts
+++ appchat/src/hooks/useForm.ts
@@ -1,14 +1,23 @@
 import React, { useState } from "react"
 
+interface FormState<T> {
+    [key: string]: T;
+}
 
-export const useForm = (initialForm) =>{
+export const useForm = <T extends FormState<string>>(initialForm: T) =>{
     const [form, setform] = useState(initialForm)
     
     const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
         const { name, value } = e.target
+
+        setform(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
     } 
 
     return {
-
+        form,
+        handleInputChange
     }
 }
