a6e62494c2a212eb456a90a32adec2c1239520bc
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 14:17:39 -0600

    [FEAT] Construyendo el componente FormWrapper
    - importando el componente MuiAvatar
    - estableciendo css en el elemento div, width del 100%, borde a 1px red solid para observar el comportamiento, usamos display flex para que el contenido este en direccion a row, padding de 1rem
    - establezco el componente MuiAvatar para observar como se ve el componente
    - establezco un div este sera el recuadro donde iran el titulo del form y dentro los TextField's

--- appchat/src/components/FormWrapper.tsx
+++ appchat/src/components/FormWrapper.tsx
@@ -1,7 +1,14 @@
+import { MuiAvatar } from "./MuiAvatar"
 
 
 export const FormWrapper = () => {
   return (
-    <div>FormWrapper</div>
+    <div style={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'row',padding:'1rem'}}>
+        <MuiAvatar/>
+
+        <div>
+
+        </div>
+    </div>
   )
 }
