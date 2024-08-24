246dbdab76d0c9de8bdcca5327a47449d6422a15
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 14:00:03 -0600

    [FEAT] Multiples cambios en el archivo MuiAvatar.tsx
    - importando el componente Avatar y Badge
    - importando el icono KeyboardArrowDownIcon
    - estableciendo el componente Badge en el retorno del componente MuiAvatar
    - el componente Badge tiene las sig props overlap el cual define la forma del componente Badge donde establecimos que sea circular la forma, badgeContent es un node donde estableci un elemento div con un icono KeyboardArrowDownIcon
    - css en el elemento div use display flex para centrar el icono, estableci un width y height de 24 unidades para que después pueda aplicarle un borderRadius del 50% y hacer el icono redondo, estableci un color de fondo rosita, margen superior de 1rem y color de borde rosita
    - establecemos el componente Avatar el cual sera nuestro nuestra imagen, css con un width y un height de 70 unidades, border-radius de 45% para poder acercarme al diseño de la imagén y un borde rosita a 3px solid
    - la url de la imagen la obtuve de randomuser

--- appchat/src/components/MuiAvatar.tsx
+++ appchat/src/components/MuiAvatar.tsx
@@ -1,7 +1,17 @@
-import { Badge } from '@mui/material';
+import { Avatar, Badge } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export const MuiAvatar = () => {
   return (
-    <div>MuiAvatar</div>
+    <Badge 
+        overlap='circular'
+        badgeContent={
+         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 24, height: 24, borderRadius: '50%', backgroundColor: '#f174dc',marginTop:'1rem',border:'1px gray solid' }}>
+            <KeyboardArrowDownIcon sx={{ fontSize:14 }} />
+          </div>
+        }
+    >
+      <Avatar sx={{width:70,height:70,borderRadius:'45%',border:'3px #f174dc solid'}} alt="juanita" src="https://randomuser.me/api/portraits/women/65.jpg" />
+    </Badge>
   )
 }
