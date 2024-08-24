0fdabae99cfd513b2185d9426e4ed416fb9d8150
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 09:12:48 -0600

    [FEAT] Estableciendo la función listen para escuchar los cambios del servidor ó las conexiones
    - establecemos el puerto donde estara escuchando la aplicación mediante las variables de entorno
    - establecemos un log con el backticks para definir un mensaje para observar que el servidor esta en funcionamiento en dicho puerto

--- apichat/src/server.ts
+++ apichat/src/server.ts
@@ -4,3 +4,8 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running in the port ${process.env.PORT}`);
+})
