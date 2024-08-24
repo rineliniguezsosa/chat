92ca2e664bdac650a7d95df324aca450d56affa3
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 10:08:42 -0600

    [FEAT] Creando el bloque try catch dentro de la función Connectdb
    - en el bloque try catch esperamos la respuesta de la conexión de la base de datos mediante la función initialize del AppDataSource
    - establecemos un log para observar la conexión a la base de datos
    - establecemos un log del error del servidor

--- apichat/src/config/connectdb.ts
+++ apichat/src/config/connectdb.ts
@@ -1,5 +1,11 @@
 import { AppDataSource } from "./datasource";
 
 export const Connectdb = async()=>{
+    try {
+        await AppDataSource.initialize()
+        console.log('successful db connection');
+    } catch (error) {
+        console.log('something went wrong',error);
     
+    }
 }
