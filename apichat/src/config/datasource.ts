bad3c5c8c4fe2ccc5c7508d74fc1a852a74b6c6f
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 10:01:26 -0600

    [FEAT] Creando el AppDataSource para establecer los parametros de conexión
    - imoportamos la entidad UserEntity para leer la entidad de la base de datos y poder realizar operaciones
    - establecemos una pequeña validación de la variable PORT, Pero no sin antes convertir el puerto en entero
    - establecemos cada una de los parametros de conexión propuestos

--- apichat/src/config/datasource.ts
+++ apichat/src/config/datasource.ts
@@ -1,1 +1,17 @@
 import { DataSource } from "typeorm"
+import { UserEntity } from "../models"
+
+
+const port = process.env.PORT ? parseInt(process.env.PORT,10) : undefined;
+
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: process.env.HOST,
+    port: port,
+    username: process.env.USERNAME,
+    password:process.env.PASSWORD,
+    database:process.env.DB,
+    synchronize: true,
+    logging: true,
+    entities:[UserEntity]
+})
