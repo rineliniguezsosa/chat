10993726cd4f641a5d32ec21a20d688d54792dd4
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 11:21:47 -0600

    [FEAT] Multiples cambios en el controlador saveUser
    - importando la entidad ó el modelo UserEntity
    - importando el  AppDataSource
    - creamos la variable data para obtener los datos del front-end mediante el req.body
    - creamos la variable userRepository  donde obtenemos del repository la entidad ó modelo UserEntity para PODER REALIZAR QUERYS O OPERACIONES en la entidad
    - esperamos la insercoión de los datos de userRepository mediante el método save y le pasamos la data por almacenar
    - establecemos un status 200 exitoso con un mensaje en formato json con el status true y con el valor de la data como mensaje
    - si la operación de insertar un registro no se cumple 1er devolvemos un log para observar el error para que revisemos el error y ante la perspectiva del usuario le devolveremos un status 500 con un mensaje para que el no sepa que sucedio, el mensaje consta de un status en false y un mensaje de error

--- apichat/src/controllers/userControllers/userControllers.ts
+++ apichat/src/controllers/userControllers/userControllers.ts
@@ -1,9 +1,23 @@
 import { Request,Response } from 'express'
+import { UserEntity } from '../../models'
+import { AppDataSource } from '../../config/datasource'
 
 export const saveUser = async(req:Request,res:Response)=>{
+    const data = req.body;
     try {
-        
+        const userRepository = AppDataSource.getRepository(UserEntity);
+
+        await userRepository.save(data)
+
+        res.status(200).json({
+            status:true,
+            message:data
+        })
     } catch (error) {
-        
+        console.log("message error:",error);
+        res.status(500).json({
+            status:false,
+            message:'Algo salio mal :(, intenta de nuevo'
+        })
     }
 }
