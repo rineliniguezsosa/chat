3d632804828e1ab6de98207b263152079853f50b
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 14:36:10 -0600

    [FEAT] Multiples cambios en el archivo FormWrapper
    - importando la interface FormWrapperProps
    -  desestructurando los props title y children y estableciendo el tipado mediantre la interface FormWrapperProps
    - css en el div, tamaño y color de borde, width del 100%, borderRadius de 10px, color de fondo #f0f0f0 este color lo obtuve mediante la extensión eye dropper, margin-left de 0.4rem
    - estableciendo el elemento h1 con el valor de la prop title
    - estableciendo el elemento form con el valor de la envoltura children

--- appchat/src/components/FormWrapper.tsx
+++ appchat/src/components/FormWrapper.tsx
@@ -1,13 +1,17 @@
+import { FormWrapperProps } from "../types/Index"
 import { MuiAvatar } from "./MuiAvatar"
 
 
-export const FormWrapper = () => {
+export const FormWrapper = ({title,children}:FormWrapperProps) => {
   return (
     <div style={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'row',padding:'1rem'}}>
         <MuiAvatar/>
 
-        <div>
-
+        <div style={{border:'1px blue solid',width:'100%',borderRadius:'10px',backgroundColor:'#f0f0f0',marginLeft:'0.4rem'}}>
+            <h2>{title}</h2>
+            <form>
+                {children}
+            </form>
         </div>
     </div>
   )
