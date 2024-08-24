7455b89a9f63076130bc785c1632de1d3f9b8cc6
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 12:12:26 -0600

    [FEAT] Cambios en el css del elemento contenedor div
    - usamos display flex para  definir el comportamiento de los elementos hijos, dirección en row y justificamos los elementos através del header

--- appchat/src/App.tsx
+++ appchat/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main>
       <header>
-        <div style={{border:'2px green solid'}}>
+        <div style={{border:'2px green solid',display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
          <div>
             <h1>Crear usuario</h1>
          </div>
