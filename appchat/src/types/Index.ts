62fb73cdab3fc7943c80dd4c6359f3abceb87d26
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 13:31:27 -0600

    [FEAT] Cambios en el archivo index.ts, creando la interface FormWrapperProps
    - importando React
    - esta interface servira para los props del FormWrapper component
    - la prop title es de tipo string y el title es el titulo del formulario que se repite
    - la prop children es tipado por un React.ReactNode el cual sera un componente ó un elemento

--- appchat/src/types/Index.ts
+++ appchat/src/types/Index.ts
@@ -1,7 +1,13 @@
 import { SxProps, Theme } from '@mui/system';
+import React from 'react';
 
 export interface LinerProgressProps {
     variant:'determinate',
     value:number,
     sx?:SxProps<Theme>
+}
+
+export interface FormWrapperProps {
+    title:string,
+    children:React.ReactNode
 }
