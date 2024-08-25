f3ab2d03d4e0dd5df0b89795842510158d1e1e60
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 15:05:48 -0600

    [FEAT] Cambiando el nombre del componente FormInformation por FormInformationCard

--- appchat/src/components/FormInformationCard.tsx
+++ appchat/src/components/FormInformationCard.tsx
@@ -1,7 +1,7 @@
 import { FormInformationProps } from "../types/Index"
 
 
-export const FormInformation = ({children}:FormInformationProps) => {
+export const FormInformationCard = ({children}:FormInformationProps) => {
   return (
     <div style={{width:'100%',border:'1px red solid',display:'flex',justifyContent:'end',padding:'1rem'}}>
     <div style={{border:'1px green solid',width:'85%',height:'auto',padding:'1rem',backgroundColor:'#f174dc',borderRadius:'9px'}}>
