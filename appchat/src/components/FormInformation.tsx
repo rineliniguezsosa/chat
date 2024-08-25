b42313abdbf2227b63631bf90bd836b8adfefbea
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 18:41:38 -0600

    [FIX] Eliminando las props title y value del componente FormInformation

--- appchat/src/components/FormInformation.tsx
+++ appchat/src/components/FormInformation.tsx
@@ -1,7 +1,7 @@
 import { FormInformationProps } from "../types/Index"
 
 
-export const FormInformation = ({title,value,children}:FormInformationProps) => {
+export const FormInformation = ({children}:FormInformationProps) => {
   return (
     <div style={{width:'100%',border:'1px red solid',display:'flex',justifyContent:'end'}}>
     <div style={{border:'1px green solid',width:'85%',height:'auto',paddingBlock:'1rem',backgroundColor:'#f174dc',borderRadius:'9px'}}>
