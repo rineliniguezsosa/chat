bbb2b544d8c681fdcbd37bee3211ebb66e515fd9
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 21:29:45 -0600

    [FEAT] Guardando los cambios del componente AllFormInformation  este componente en cuanto a contenido es casi similar al de FormInformation
    - importando la interface FormInformationProps
    - reutilice el mismo tipado que el de FormInformationProps

--- appchat/src/components/AllFormInformation.tsx
+++ appchat/src/components/AllFormInformation.tsx
@@ -1,7 +1,12 @@
+import { FormInformationProps } from "../types/Index"
 
 
-export const AllFormInformation = () => {
+export const AllFormInformation = ({children}:FormInformationProps) => {
   return (
-    <div>AllFormInformation</div>
+    <div style={{width:'100%',border:'1px red solid',padding:'1rem'}}>
+    <div style={{border:'1px green solid',width:'100%',height:'auto',padding:'1rem',backgroundColor:'#f174dc',borderRadius:'9px'}}>
+      {children}
+    </div>
+  </div>
   )
 }
