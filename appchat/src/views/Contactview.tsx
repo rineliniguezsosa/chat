03f6c7fe3dee33b0ca74a3fb3f036366bca9c8b5
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 12:53:59 -0600

    [FEAT] Creando el archivo Contactview.tsx
    - en este archivo voy a separar el contenido voy a tener que pasar por props el valor del form y la referencia del evento
    - hice una prueba y me di cuenta que estaba tipando mal la interface para pasar la prop de form, afortunademente realizar las pruebas que realizo funcionan como espero
    - como guardar el archivo Contactview es una prueba que salio como esperaba
    - importando la interface ContactviewProps
    - creando el funcional componente Contactview
    - desestructurando el prop form y tipandolo por la interface ContactviewProps
    - estableciendo el log para obsrevar los cambios
    - devolviendo un h1 por el momento, aqui voy a mover el contenido del form inicial

---
+++ appchat/src/views/Contactview.tsx
@@ -0,0 +1,10 @@
+
+import { ContactviewProps } from '../types/Index'
+
+export const Contactview = ({form}:ContactviewProps) => {
+    console.log(form);
+    
+  return (
+    <div>Contact</div>
+  )
+}
