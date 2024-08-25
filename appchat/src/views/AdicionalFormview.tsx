7b888c78a3db4b09c67b1c34d3f8942c35d294a1
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 13:28:31 -0600

    [FEAT] Cambios en el archivo AdicionalFormview
    - importando los componentes FormWrapper y MuiTextField
    - importando la interface ContactviewProps
    - desestructurando las props form y handleInputChange
    - pegando el contenido del formulario de los datos del nombre en el archivo AdicionalFormview

--- appchat/src/views/AdicionalFormview.tsx
+++ appchat/src/views/AdicionalFormview.tsx
@@ -1,7 +1,50 @@
+import { FormWrapper } from "../components/FormWrapper"
+import { MuiTextField } from "../components/MuiTextField"
+import { ContactviewProps } from "../types/Index"
 
 
-export const AdicionalFormview = () => {
+export const AdicionalFormview = ({form,handleInputChange}:ContactviewProps) => {
   return (
-    <div>AdicionalFormview</div>
+    <FormWrapper title='¿Cual es tu nombre?'>
+          <MuiTextField
+            name='name'
+            type='text'
+            label='Nombre'
+            variant='outlined'
+            value={form.name}
+            sx={{width:'100%',marginBottom:'1rem'}}
+            onChange={handleInputChange}
+          />
+
+          <MuiTextField
+            name='middlename'
+            type='text'
+            label='Segundo nombre'
+            variant='outlined'
+            value={form.middlename}
+            sx={{width:'100%',marginBottom:'1rem'}}
+            onChange={handleInputChange}
+          />
+
+          <MuiTextField
+            name='paternalsurname'
+            type='text'
+            label='Apellido paterno'
+            variant='outlined'
+            value={form.paternalsurname}
+            sx={{width:'100%',marginBottom:'1rem'}}
+            onChange={handleInputChange}
+          />
+
+          <MuiTextField
+            name='maternalsurname'
+            type='text'
+            label='Apellido materno'
+            variant='outlined'
+            value={form.maternalsurname}
+            sx={{width:'100%',marginBottom:'1rem'}}
+            onChange={handleInputChange}
+          />
+      </FormWrapper>
   )
 }
