e4fe79439492ac504f627602aeb874ec9f4b8f0c
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 18:53:08 -0600

    [FEAT] Cambios de css en el componente FormInformation
    - padding de 1rem en el elemento hijo div

--- appchat/src/components/FormInformation.tsx
+++ appchat/src/components/FormInformation.tsx
@@ -4,7 +4,7 @@ import { FormInformationProps } from "../types/Index"
 export const FormInformation = ({children}:FormInformationProps) => {
   return (
     <div style={{width:'100%',border:'1px red solid',display:'flex',justifyContent:'end'}}>
-    <div style={{border:'1px green solid',width:'85%',height:'auto',paddingBlock:'1rem',backgroundColor:'#f174dc',borderRadius:'9px'}}>
+    <div style={{border:'1px green solid',width:'85%',height:'auto',padding:'1rem',backgroundColor:'#f174dc',borderRadius:'9px'}}>
       {children}
     </div>
   </div>
