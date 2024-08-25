b98778fdfc848207ee5a854f2e46d16ca1380019
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-24 18:38:39 -0600

    [FEAT] Cambios en el componente FormInformation
    - importando al interaface FormInformationProps
    - establecemos css en el elemento div principal del componente FormInformation, width del 100%, tamaño y color de borde, usamos display flex para justifiucar el contenido al final
    - css en el div hijo, tamaño y color de borde, width del 85%, height auto, paddingBlock interno de 1rem, background rosita, border-radius de 9px

--- appchat/src/components/FormInformation.tsx
+++ appchat/src/components/FormInformation.tsx
@@ -1,7 +1,12 @@
+import { FormInformationProps } from "../types/Index"
 
 
-export const FormInformation = () => {
+export const FormInformation = ({title,value,children}:FormInformationProps) => {
   return (
-    <div>FormInformation</div>
+    <div style={{width:'100%',border:'1px red solid',display:'flex',justifyContent:'end'}}>
+    <div style={{border:'1px green solid',width:'85%',height:'auto',paddingBlock:'1rem',backgroundColor:'#f174dc',borderRadius:'9px'}}>
+      {children}
+    </div>
+  </div>
   )
 }
