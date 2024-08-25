7a6ad15e712ba4c46bf9e1a492c1f412566d61e4
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 15:32:57 -0600

    [FEAT] Cambios en el componente AllFormInformation.tsx
    - estableciendo  un padding interno de 2rem para el contenedotr div
    - estableciendo estilos de funete en el contenedor div, fuente roboto, fontweight a 500, fontsttyle normal

--- appchat/src/components/AllFormInformation.tsx
+++ appchat/src/components/AllFormInformation.tsx
@@ -4,7 +4,7 @@ import { FormInformationProps } from "../types/Index"
 export const AllFormInformation = ({children}:FormInformationProps) => {
   return (
     <div style={{width:'100%',border:'1px red solid',padding:'1rem'}}>
-    <div style={{border:'1px green solid',width:'100%',height:'auto',padding:'1rem',backgroundColor:'#f174dc',borderRadius:'9px'}}>
+    <div style={{border:'1px green solid',width:'100%',height:'auto',padding:'2rem',backgroundColor:'#f174dc',borderRadius:'9px',fontFamily: '"Roboto", sans-serif',fontWeight:500,fontStyle:'normal'}}>
       {children}
     </div>
   </div>
