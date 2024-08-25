6c269e785ce72fe25ff50371741f9797bb6acc99
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 13:05:17 -0600

    [FEAT] Cambios en el archivo Contactview
    - importando los componentes FormWrapper  y MuiTextField
    - desestructurando la prop handleInputChange
    - eliminando el log
    - pegando el contenido de los datos de contacto en la vista ContactView

--- appchat/src/views/Contactview.tsx
+++ appchat/src/views/Contactview.tsx
@@ -1,10 +1,30 @@
 
+import { FormWrapper } from '../components/FormWrapper';
+import { MuiTextField } from '../components/MuiTextField';
 import { ContactviewProps } from '../types/Index'
 
-export const Contactview = ({form}:ContactviewProps) => {
-    console.log(form);
-    
+export const Contactview = ({form,handleInputChange}:ContactviewProps) => {
   return (
-    <div>Contact</div>
+    <FormWrapper title='Datos de contacto'>
+            <MuiTextField
+              name='email'
+              type='email'
+              label='Correo electronico'
+              variant='outlined'
+              value={form.email}
+              sx={{width:'100%',marginBottom:'1rem'}}
+              onChange={handleInputChange}
+            />
+
+            <MuiTextField
+              name='phone'
+              type='tel'
+              label='Teléfono celular'
+              variant='outlined'
+              value={form.phone}
+              sx={{width:'100%',marginBottom:'1rem'}}
+              onChange={handleInputChange}
+            />
+        </FormWrapper>
   )
 }
