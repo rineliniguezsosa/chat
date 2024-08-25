580b49b8c7d705d0ae0dadbeae9f20634d6f6467
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 14:10:51 -0600

    [FEAT] Cambios en el archivo AdicionalFormview.tsx
    - envolviendo por un fragment el contenido del componente AdicionalFormview
    . estableciendo el formulario para la fecha de nacimiento

--- appchat/src/views/AdicionalFormview.tsx
+++ appchat/src/views/AdicionalFormview.tsx
@@ -5,46 +5,73 @@ import { ContactviewProps } from "../types/Index"
 
 export const AdicionalFormview = ({form,handleInputChange}:ContactviewProps) => {
   return (
-    <FormWrapper title='¿Cual es tu nombre?'>
+    <>
+        <FormWrapper title='¿Cual es tu nombre?'>
           <MuiTextField
-            name='name'
-            type='text'
-            label='Nombre'
-            variant='outlined'
-            value={form.name}
-            sx={{width:'100%',marginBottom:'1rem'}}
-            onChange={handleInputChange}
-          />
+              name='name'
+              type='text'
+              label='Nombre'
+              variant='outlined'
+              value={form.name}
+              sx={{ width: '100%', marginBottom: '1rem' }}
+              onChange={handleInputChange} />
 
           <MuiTextField
-            name='middlename'
-            type='text'
-            label='Segundo nombre'
-            variant='outlined'
-            value={form.middlename}
-            sx={{width:'100%',marginBottom:'1rem'}}
-            onChange={handleInputChange}
-          />
+              name='middlename'
+              type='text'
+              label='Segundo nombre'
+              variant='outlined'
+              value={form.middlename}
+              sx={{ width: '100%', marginBottom: '1rem' }}
+              onChange={handleInputChange} />
 
           <MuiTextField
-            name='paternalsurname'
-            type='text'
-            label='Apellido paterno'
-            variant='outlined'
-            value={form.paternalsurname}
-            sx={{width:'100%',marginBottom:'1rem'}}
-            onChange={handleInputChange}
-          />
+              name='paternalsurname'
+              type='text'
+              label='Apellido paterno'
+              variant='outlined'
+              value={form.paternalsurname}
+              sx={{ width: '100%', marginBottom: '1rem' }}
+              onChange={handleInputChange} />
 
           <MuiTextField
-            name='maternalsurname'
-            type='text'
-            label='Apellido materno'
-            variant='outlined'
-            value={form.maternalsurname}
-            sx={{width:'100%',marginBottom:'1rem'}}
-            onChange={handleInputChange}
-          />
-      </FormWrapper>
+              name='maternalsurname'
+              type='text'
+              label='Apellido materno'
+              variant='outlined'
+              value={form.maternalsurname}
+              sx={{ width: '100%', marginBottom: '1rem' }}
+              onChange={handleInputChange} />
+        </FormWrapper>
+
+        <FormWrapper title='¿Cúal es tu fecha de nacimiento?'>
+              <MuiTextField
+                  name='day'
+                  type='text'
+                  label='Día'
+                  variant='outlined'
+                  value={form.day}
+                  sx={{ width: '100%', marginBottom: '1rem' }}
+                  onChange={handleInputChange} />
+
+              <MuiTextField
+                  name='month'
+                  type='text'
+                  label='Mes'
+                  variant='outlined'
+                  value={form.month}
+                  sx={{ width: '100%', marginBottom: '1rem' }}
+                  onChange={handleInputChange} />
+
+              <MuiTextField
+                  name='year'
+                  type='text'
+                  label='Año'
+                  variant='outlined'
+                  value={form.year}
+                  sx={{ width: '100%', marginBottom: '1rem' }}
+                  onChange={handleInputChange} />
+        </FormWrapper>
+    </>
   )
 }
