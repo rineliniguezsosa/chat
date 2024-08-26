e16dcc211cf796b09fdcb403948b9d27969cdef7
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 20:16:52 -0600

    [FEAT] Eliminando bordes

--- appchat/src/components/FormWrapper.tsx
+++ appchat/src/components/FormWrapper.tsx
@@ -4,10 +4,10 @@ import { MuiAvatar } from "./MuiAvatar"
 
 export const FormWrapper = ({title,children}:FormWrapperProps) => {
   return (
-    <div style={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'row',padding:'1rem',marginTop:'2rem'}}>
+    <div style={{width:'100%',display:'flex',flexDirection:'row',padding:'1rem',marginTop:'2rem'}}>
         <MuiAvatar/>
 
-        <div style={{border:'1px blue solid',width:'100%',borderRadius:'10px',backgroundColor:'#f0f0f0',marginLeft:'0.4rem',padding:'0.6rem'}}>
+        <div style={{width:'100%',borderRadius:'10px',backgroundColor:'#f0f0f0',marginLeft:'0.4rem',padding:'0.6rem'}}>
             <h2 style={{paddingBottom:'1rem',fontFamily: '"Roboto", sans-serif',fontWeight:700,fontStyle:'normal'}}>{title}</h2>
             <form>
                 {children}
