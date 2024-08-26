32ef8372f29acef2078eca94af16098181ea5c77
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-08-25 20:16:47 -0600

    [FEAT] Eliminando bordes

--- appchat/src/components/FormInformationCard.tsx
+++ appchat/src/components/FormInformationCard.tsx
@@ -3,8 +3,8 @@ import { FormInformationProps } from "../types/Index"
 
 export const FormInformationCard = ({children}:FormInformationProps) => {
   return (
-    <div style={{width:'100%',border:'1px red solid',display:'flex',justifyContent:'end',padding:'1rem'}}>
-    <div style={{border:'1px green solid',width:'85%',height:'auto',padding:'1rem',backgroundColor:'#f174dc',borderRadius:'9px',fontFamily: '"Roboto", sans-serif',fontWeight:500,fontStyle:'normal'}}>
+    <div style={{width:'100%',display:'flex',justifyContent:'end',padding:'1rem'}}>
+    <div style={{width:'85%',height:'auto',padding:'1rem',backgroundColor:'#f174dc',borderRadius:'9px',fontFamily: '"Roboto", sans-serif',fontWeight:500,fontStyle:'normal'}}>
       {children}
     </div>
   </div>
